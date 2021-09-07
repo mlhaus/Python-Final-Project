a786186b04779012d9b943192caf035aa8a55e40
Marc Hauschildt <marc.hauschildt@gmail.com>
2021-09-07 16:19:54 -0500

    Learned about print()

---
+++ Chapter2/program2-1.py
@@ -0,0 +1,11 @@
+print("*** Program 2-1, Page 37, and Programs 2-5 and 2-6, Page 39 ***")
+# This program displays a person's name and address
+print("Marc Hauschildt")        # Name
+print('123 Full Circle Drive')  # Street address
+print('Asheville, NC 28899')    # City, state, and ZIP
+'''
+print() is a function that requires input (called an argument) inside the parenthesis
+A single string literal can be enclosed in single quotes
+Single-line comments start with a hash tag
+Multiple-line comments are wrapped inside a set of triple quotation marks.
+'''
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Python-Final-Project
+# Final Project
+This is the final project for my CIS-207 Fundamentals of Web Programming course at [Kirkwood Community College](https://www.kirkwood.edu). 
+## Introduction
+Write your reflection here.
